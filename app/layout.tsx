c633648731a0c99f0349dff50d3f91ae073afa48
clnt <c.connery14@gmail.com>
2024-11-02 10:46:18 +0100

    navbar creation and theming configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 import React from "react";
+
+import Navbar from "@/components/navigation/navbar";
 
 const Inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -30,11 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${Inter.className} ${SpaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem={true}
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
