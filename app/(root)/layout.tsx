ac444848aee3514d655814c91a3e9f4d926aa923
clnt <c.connery14@gmail.com>
2024-11-03 12:22:21 +0100

    implmenting Oauth with github and google.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "@/components/navigation/navbar";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,22 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
+    </>
+  );
+}
