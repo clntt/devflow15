8d321c2cb3d3056e6ee1f4a4b0b01092f13a6eb6
clnt <c.connery14@gmail.com>
2024-11-08 11:05:01 +0100

    implement ui of the QuestionForm

--- lib/validations.ts
+++ lib/validations.ts
@@ -55,3 +55,21 @@ export const SignUpSchema = z.object({
       message: "Password must contain atleast one number.",
     }),
 });
+
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title is required." })
+    .max(100, { message: "Title must be at most 100 characters long." }),
+
+  content: z.string().min(1, { message: "Body is required." }),
+  tags: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag must be at most 30 characters long." })
+    )
+    .min(1, { message: "Atleast one tag is required." })
+    .max(3, { message: "Atmost 5 tags are allowed." }),
+});
