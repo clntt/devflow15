2def6e55ad0317c0feb41160dadf14df5004b9a1
clnt <c.connery14@gmail.com>
2024-11-10 12:48:45 +0100

    vote model

---
+++ database/vote.model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, models, Types } from "mongoose";
+
+export interface IVote {
+  author: Types.ObjectId;
+  id: Types.ObjectId;
+  type: "question" | "answer";
+  voteType: "upvote" | "downvote";
+}
+
+export const VoteSchema = new Schema<IVote>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    id: { type: Schema.Types.ObjectId, required: true },
+    type: { type: String, enum: ["question", "answer"], required: true },
+    voteType: { type: String, enum: ["upvote", "downvote"], required: true },
+  },
+  { timestamps: true }
+);
+
+export const Vote = models?.vote || model<IVote>("Vote", VoteSchema);
+
+export default Vote;
